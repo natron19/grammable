accebe86d59ff8e3bcbbfd6d7d27464b0085577a
Nathan Rohm <nathanrohm@gmail.com>
2016-10-07 23:51:02 +0000

    adds create message with test

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,4 +2,22 @@ class GramsController < ApplicationController
   def index
   end
 
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.new(gram_params)
+    if @gram.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 end
