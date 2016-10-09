54ec394ac430386171cbeaf82d2e5378925f4054
Nathan Rohm <nathanrohm@gmail.com>
2016-10-09 02:15:27 +0000

    adds files

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,25 @@
 class GramsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :current_gram, only: [:show, :edit, :update]
 
   def index
     @grams = Gram.all
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @gram.update_attributes(gram_params)
+      redirect_to root_path, notice: "Gram updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -20,6 +36,15 @@ class GramsController < ApplicationController
   end
 
   private
+
+  def current_gram
+    @gram ||= Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def render_not_found
+    render text: "Not Found", status: :not_found
+  end
 
   def gram_params
     params.require(:gram).permit(:message)
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,109 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#show action" do
+
+    it "should successfully show the page if the gram is found" do
+
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+      gram = Gram.create(
+        message: "hello",
+        user_id: user.id
+      )
+
+      get :show, id: gram.id
+      expect(response).to have_http_status(:success)
+
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, id: 'TACOCAT'
+      expect(response).to have_http_status(:not_found)
+    end
+
+  end
+
+
+  describe "grams#edit action" do
+
+    it "should successfully show the edit page if the gram is found" do
+
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+      gram = Gram.create(
+        message: "hello",
+        user_id: user.id
+      )
+
+      get :edit, id: gram.id
+      expect(response).to have_http_status(:success)
+
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :edit, id: 'SWAG'
+      expect(response).to have_http_status(:not_found)
+    end
+
+  end
+
+  describe "grams#update action" do
+
+    it "should allow users to successfully update grams" do
+
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+      p = Gram.create(
+        message: "initial value",
+        user_id: user.id
+      )
+
+      patch :update, id: p.id, gram: { message: 'Changed' }
+      expect(response).to redirect_to root_path
+
+      p.reload
+      expect(p.message).to eq "Changed"
+
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      patch :update, id: "YOLOSWAG", gram: {message: "changed value"}
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessabile_entity" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      p = Gram.create(
+        message: "Initial Value",
+        user_id: user.id
+      )
+
+      patch :update, id: p.id, gram: { message: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      p.reload
+      expect(p.message).to eq "Initial Value"
+    end
+
+  end
+
   describe "grams#index action " do
     it "should successfully show the page" do
       get :index
